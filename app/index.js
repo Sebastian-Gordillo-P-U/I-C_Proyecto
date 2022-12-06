f28a57cc200c625ef73348b02ae5125b3db36af6
Sebastian <jsgordillo75@misena.edu.co>
2022-12-05 21:33:41 -0500

    Archivo Correr

--- app/index.js
+++ app/index.js
@@ -7,7 +7,9 @@ app.get('/', function (req, res) {
   res.sendFile(ruta.resolve(__dirname, 'API_uno.html'))
 });
 
-
+server.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 // var server = app.listen(3000, '0.0.0.0', function () {
 
 //   var host = server.address().address;
