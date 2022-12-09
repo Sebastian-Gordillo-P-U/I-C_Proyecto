2f70983826a731122073a35dd31b0d2b435c0772
Sebastian <jsgordillo75@misena.edu.co>
2022-12-08 19:58:18 -0500

    Conexión Base de Datos

--- app/index.js
+++ app/index.js
@@ -1,15 +1,13 @@
 const mariadb = require('mariadb');
-const ruta = require('path')
+// const ruta = require('path')
 var express = require('express');
 var app = express();
-const express = require('express');
-const app = express();
-const port = 3000;
+// const port = 3306;
 
-app.use(express.json());
-app.listen(port, ()=>{
-	console.log('Server is listening on port',port,'...');
-});
+// app.use(express.json());
+// app.listen(port, ()=>{
+// 	console.log('Server is listening on port',port,'...');
+// });
 
 //Conexión Base de Datos
 const conexion_BD = mariadb.createPool({
@@ -20,17 +18,48 @@ const conexion_BD = mariadb.createPool({
   database: 'Mini'
 });
 
-async function solicitudConexion (){
+async function solicitudConexion() {
   try {
-    console.log('Conexión exitosa')
-    const conexionBD = await conexion_BD.getConnection();
+    const conexionBD = await conexion_BD.solicitudConexion();
     return conexionBD;
   } catch (error) {
     console.log(error);
   }
 }
 
-solicitudConexion()
+app.get('/customers', async (req, res)=>{
+  try{
+      //Conexión
+      const conn = await conexion_BD.solicitudConexion();
+      //Consulta
+      const consulta = 'select * from customers';
+      //Ejecuciçon consulta
+      const camposBD = await conn.query(consulta);
+      //Respuesta
+      res.status(200).json(camposBD)
+  }catch(error){
+    console.log(error)
+  }
+});
+
+app.post('/nuevoUsuario', async (req, res) =>{
+    try{
+      //Conexión
+      const conn = await conexion_BD.solicitudConexion();
+      //Consulta
+      const consulta = 'INSERT INTO products(name) SET (?)';
+      //Ejecuciçon consulta
+      const resultado = await conn.query(consulta, [req.body.name]);
+      //Respuesta
+      res.status(200).json(resultado)
+    }catch(error){
+      console.log(error)
+    }
+})
+
+app.listen(3306, () => {
+  console.log('Servicio', 3306)
+});
 
 // var server = app.listen(3000, '0.0.0.0', function () {
 
@@ -39,9 +68,9 @@ solicitudConexion()
 
 //   console.log('Example app listening at http://%s:%s', host, port);
 // });
-app.get('/customers', (req,res)=>{
-	res.send(
-		{ id: 1, name: "Ramón", },
-	);
-});
+// app.get('/customers', (req,res)=>{
+// 	res.send(
+// 		{ id: 1, name: "Ramón", },
+// 	);
+// });
 
